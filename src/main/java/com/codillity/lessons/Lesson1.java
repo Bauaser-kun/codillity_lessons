bcb71be4f844bc997e944796e82f4de35cf9704c
Bauaser <michal.bala.1989@gmail.com>
2021-01-16 15:35:35 +0100

    first lesson - binary gap

--- src/main/java/com/codillity/lessons/Lesson1.java
+++ src/main/java/com/codillity/lessons/Lesson1.java
@@ -1,4 +1,29 @@
 package com.codillity.lessons;
 
+//result with this code 100%
 public class Lesson1 {
+    public int solution(int N) {
+        int gap = 0;
+
+        if (N == 0) {
+            return 0;
+        }
+
+        while (N%2 == 0) {
+            N = N/2;
+        }
+
+        for (int i = 0; N > 0; N /=2) {
+            if (N%2 == 0) {
+                i++;
+            } else {
+                if (i > gap) {
+                    gap = i;
+                }
+                i = 0;
+            }
+        }
+
+        return gap;
+    }
 }
