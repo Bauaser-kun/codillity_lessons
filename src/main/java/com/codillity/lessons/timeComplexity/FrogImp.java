bffc5272f6078bf3dfc6d825020f859dc81fef82
Bauaser <michal.bala.1989@gmail.com>
2021-01-18 20:52:29 +0100

    lesson 3.1 FrogJmp update to 100% score

--- src/main/java/com/codillity/lessons/timeComplexity/FrogImp.java
+++ src/main/java/com/codillity/lessons/timeComplexity/FrogImp.java
@@ -1,8 +1,20 @@
 package com.codillity.lessons.timeComplexity;
 
-//With this code got 44%
+//With this code got 100% overall score
 public class FrogImp {
-    public int solution(int X, int Y, int D) {
+     public int solution(int X, int Y, int D) {
+        if (X == Y) {
+            return 0;
+        } else if ((Y - X)%D == 0) {
+            return ((Y - X)/D);
+        } else {
+            return ((Y - X)/D) + 1;
+        }
+    }
+
+    //this gave 100% correct answers but takes to
+    //long - leading to 44% overall score of lesson
+    public int solution2(int X, int Y, int D) {
         int jumps = 0;
 
         while (X < Y){
@@ -13,3 +25,5 @@ public class FrogImp {
         return jumps;
     }
 }
+
+
