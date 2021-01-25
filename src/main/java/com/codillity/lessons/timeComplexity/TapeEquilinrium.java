f553876c2defa93bb2f10de2859e71c5ac3f4f76
Bauaser <michal.bala.1989@gmail.com>
2021-01-25 16:56:28 +0100

    Lesson 3.3 100% update

--- src/main/java/com/codillity/lessons/timeComplexity/TapeEquilinrium.java
+++ src/main/java/com/codillity/lessons/timeComplexity/TapeEquilinrium.java
@@ -1,42 +1,39 @@
 package com.codillity.lessons.timeComplexity;
 
-//With this code got 46% overall score
-//85% Correctnes 0% performance
-//wrong answer on small elements with only one positive value
-//timeout on everything
-//Detected time complexity: O(N * N)
+//With this code got 100% overall score
+
 public class TapeEquilinrium {
     public int solution(int[] A) {
-        int sum1 = 0;
+        int sum1 = A[0];
         int sum2 = 0;
-        int minimalDifference = 1000;
+        int minimalDifference = 10000;
+        int checkedDifference = 0;
 
         if (A.length == 2) {
-            if (A[0] < 0 && A[1] > 0) {
-                return Math.abs(A[0]) + A[1];
-            } else if (A[0] > 0 && A[1] < 0) {
-                return Math.abs(A[1]) + A[0];
-            } else {
-                return Math.abs(A[0] - A[1]);
-            }
+          if (A[0] < 0 && A[1] > 0) {
+              return Math.abs(A[0]) + A[1];
+          } else if (A[0] > 0 && A[1] < 0) {
+              return Math.abs(A[1]) + A[0];
+          } else {
+              return Math.abs(A[0] - A[1]);
+          }
         }
 
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j <= i; j++) {
-                sum1 += A[j];
-            }
-
-            for (int k = i+1; k <A.length; k++) {
-                sum2 += A[k];
-            }
-
-            if (Math.abs(sum1 - sum2) < minimalDifference) {
-                minimalDifference = Math.abs(sum1 - sum2);
-            }
-
-            sum1 = 0;
-            sum2 = 0;
+        for (int i = 1; i < A.length; i++) {
+            sum2 +=A[i];
         }
+
+        for (int i = 1; i < A.length; i++){
+            checkedDifference = Math.abs(sum1 - sum2);
+
+            if (checkedDifference < minimalDifference){
+                minimalDifference = checkedDifference;
+            }
+
+            sum1 += A[i];
+            sum2 -= A[i];
+        }
+
         return minimalDifference;
     }
 }
